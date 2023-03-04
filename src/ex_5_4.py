a35d8ee3980354f6cf252e7bc5a96c7d9fde9a65
nikhilpatel6075 <123262574+nikhilpatel6075@users.noreply.github.com>
2023-03-03 21:34:57 -0600

    Update ex_5_4.py

--- src/ex_5_4.py
+++ src/ex_5_4.py
@@ -16,6 +16,8 @@ output_dir = root_dir / "outputs"
 input_file = data_dir / "ex_5_4-data.csv"
 output_file = output_dir / "ex_5_4-processed.csv"
 
-# Process the input data using numpy
+textfl = np.loadtxt(input_file)
 
-# Save the result to output_file
+textfl[textfl<0] = 0
+
+np.savetxt(output_file, textfl, fmt='%.2e')
